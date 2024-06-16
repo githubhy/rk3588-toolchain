d5372a3fe5d0f61482c02c7775223f02ef21d8b5
Asher Huang <huangyea@gmail.com>
2024-06-16 08:37:59 +0800

    update test script. [no ci]

--- mk_temp.sh
+++ mk_temp.sh
@@ -1,3 +1,2 @@
-mkdir temp
-cd temp
-truncate -s 5G 1.img 2.img 3.img 4.img 5.img 6.img
+mkdir temp && cd temp && truncate -s 5G 1.img 2.img 3.img 4.img 5.img 6.img
+# DATE=$(date +%Y-%m-%d_%H-%M-%S); touch ${DATE}.log
